4ef727baab95a7550c56791db7affa785fd2b4df
Cesar Val Leiva <cesarvalleiva@gmail.com>
2021-12-27 20:12:07 -0300

    hook useState

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import "./Navbar.css"
 
 const Navbar = () => {
+    const [nombre, setNombre] = useState('Rodolfo');
+
+    console.log(`Nombre: ${nombre}`)
+
+    const cambiarNombre = () => {
+        setNombre('Ocatvio');
+    }
+
     return ( 
-        <h1 className='titulo'>
-            Este es el navbar
-        </h1>
+        <>
+            <h1 className='titulo'>
+                Este es el navbar de {nombre}
+            </h1>
+            <button className='btn btn-success' onClick={() => setNombre("Hectos")}>
+                Cambiar nombre
+            </button>
+        </>
      );
 }
  
