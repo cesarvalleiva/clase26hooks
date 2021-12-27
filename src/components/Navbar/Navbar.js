6fa458cf538407d10cbbf1910d9fb8b7ae413269
Cesar Val Leiva <cesarvalleiva@gmail.com>
2021-12-27 20:25:18 -0300

    hook useEffect

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import "./Navbar.css"
 
 const Navbar = () => {
     const [nombre, setNombre] = useState('Rodolfo');
+    const [apellido, setApellido] = useState('Lopez')
 
     console.log(`Nombre: ${nombre}`)
+    console.log(`Apellido: ${apellido}`)
 
     const cambiarNombre = () => {
-        setNombre('Ocatvio');
+        setNombre('Jorge');
     }
+
+    useEffect(() => {
+        setNombre('Ocatvio');
+    }, [apellido])
 
     return ( 
         <>
             <h1 className='titulo'>
                 Este es el navbar de {nombre}
             </h1>
-            <button className='btn btn-success' onClick={() => setNombre("Hectos")}>
+            <button className='btn btn-success' onClick={cambiarNombre}>
                 Cambiar nombre
+            </button>
+
+            <button className='btn btn-danger ms-2' onClick={() => setApellido('Garcia')}>
+                Cambiar apellido
             </button>
         </>
      );
